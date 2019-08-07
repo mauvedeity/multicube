f878adaf3032ff3c7a3bf4e972cdecbb86793aab
Rhys Wilkins <rhys.wilkins@sainsburys.co.uk>
2019-08-07 10:43:34 +0100

    Single-threaded version done

--- multicube.py
+++ multicube.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 """
                  _ _   _            _                        
  _ __ ___  _   _| | |_(_) ___ _   _| |__   ___   _ __  _   _ 
@@ -10,3 +12,27 @@ this is multicube - an attempt to do the cubesum problem in a
 multithreaded environment.
 """
 
+def cube(n):
+  return(n*n*n)
+
+def check(h,t,u):
+  cusum = cube(h) + cube(t) + cube(u)
+  num = (h*100) + (t*10)+u
+  return(cusum == num, num)
+
+def tucube(h):
+  num = 0
+  for t in range(10):
+    for u in range(10):
+      hit, num = check(h, t, u)
+      if hit:
+        print(num)
+
+def process():
+  for i in range(1,10):
+    print(i)
+    tucube(i)
+
+if __name__ == '__main__':
+  process()
+      
